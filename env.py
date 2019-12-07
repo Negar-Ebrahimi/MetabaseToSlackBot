a82fdf8beb1f3c9129004b9ca618dac708bce8cf
Negar Ebrahimi <negarfreebots@gmail.com>
2019-12-07 17:37:48 +0330

    Add env file to store constantsseparated from the engine.

---
+++ env.py
@@ -0,0 +1,9 @@
+METABOT_TOKEN = "xoxb-810993959495-844405375523-eI7WEBHDhskghHH05pUb8IKK"
+
+CARD_ID_ACCOMMODATION = "162"
+CARD_ID_TOTAL = "194"
+ 
+SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
+SLACK_API_LOOK_UP_BY_EMAIL = "https://slack.com/api/users.lookupByEmail"
+
+EMAIL_NEGAR = "ng.ebrahimi@jabama.com"
