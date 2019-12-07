40749264a80cdf31fa0b368df0aba13d16054833
Negar Ebrahimi <negarfreebots@gmail.com>
2019-12-07 18:30:45 +0330

    Add Hossein's Email.

--- env.py
+++ env.py
@@ -7,3 +7,4 @@ SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 SLACK_API_LOOK_UP_BY_EMAIL = "https://slack.com/api/users.lookupByEmail"
 
 EMAIL_NEGAR = "ng.ebrahimi@jabama.com"
+EMAIL_HOSSEIN = "h.mehdikhah@Alibaba.ir"
