cf073f691384655744b66de75bfc1fdd13b0937c
negar_ebrhmi <ng.ebrahimi@jabama.com>
2020-01-01 14:59:29 +0330

    Add Metabase session and search card.

--- env.py
+++ env.py
@@ -1,7 +1,10 @@
+METABASE_SESSION = "4605c08f-5feb-4319-89bb-6929fc01a87f"
+
 METABOT_TOKEN = "xoxb-810993959495-844405375523-eI7WEBHDhskghHH05pUb8IKK"
 
 CARD_ID_ACCOMMODATION = "162"
 CARD_ID_TOTAL = "194"
+CARD_ID_SEARCH = "418"
  
 SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 SLACK_API_LOOK_UP_BY_EMAIL = "https://slack.com/api/users.lookupByEmail"
