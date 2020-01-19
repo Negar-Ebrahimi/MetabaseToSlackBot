f84831e64991e5afb77899c5bec98e77879604cc
negar_ebrhmi <ng.ebrahimi@jabama.com>
2020-01-19 14:28:00 +0330

    Add email addresses and Negar's username and password.

--- env.py
+++ env.py
@@ -1,13 +1,22 @@
-METABASE_SESSION = "4605c08f-5feb-4319-89bb-6929fc01a87f"
-
 METABOT_TOKEN = "xoxb-810993959495-844405375523-eI7WEBHDhskghHH05pUb8IKK"
+
+METABASE_USERNAME_NEGAR = "negarfreebots@gmail.com"
+METABASE_PASSWORD_NEGAR = "9AXSlOIqyDkRJO"
+
 
 CARD_ID_ACCOMMODATION = "162"
 CARD_ID_TOTAL = "194"
 CARD_ID_SEARCH = "418"
- 
+
+METABASE_API_GET_SESSION = "http://metabase.jabama.com/api/session" 
+
 SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 SLACK_API_LOOK_UP_BY_EMAIL = "https://slack.com/api/users.lookupByEmail"
 
-EMAIL_NEGAR = "ng.ebrahimi@jabama.com"
 EMAIL_HOSSEIN = "h.mehdikhah@Alibaba.ir"
+EMAIL_AHMAD = "ahmad@jabama.ir"
+EMAIL_NIMA_EBRAHIMI = "n.ebrahimi@jabama.ir"
+EMAIL_NIMA_RASOULZADE = "n.rasoulzade@jabama.ir"
+EMAIL_MASOUD = "mm.shiri@Alibaba.ir"
+EMAIL_SAMA = "s.ostadi@Jabama.ir"
+EMAIL_MOHAMMAD_AMIRI = "Mh.Amiri@Jabama.com"
