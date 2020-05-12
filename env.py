193bd4c30a5941ecfbcb9dca638cdc1429d2d8ef
negaR Ebrahimi <negarfreebots@gmail.com>
2020-05-12 14:47:36 +0430

    Change metabot job time, Add a new Card to report the orders count grouped by latest status.

--- env.py
+++ env.py
@@ -4,18 +4,22 @@ METABASE_USERNAME = "metaappslack@gmail.com"
 METABASE_PASSWORD = "2X_I3rgeEOSlCZ"
 
 CARD_ID_ACCOMMODATION = "162"
+CARD_ID_ACCOMMODATION_BY_VENUE = "1551"
 CARD_ID_TOTAL = "194"
 CARD_ID_SEARCH = "418"
+CARD_ID_ORDER_STATUS = "42"
+
 
 METABASE_API_GET_SESSION = "http://metabase.jabama.com/api/session" 
 
 SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 SLACK_API_LOOK_UP_BY_EMAIL = "https://slack.com/api/users.lookupByEmail"
 
+EMAIL_NEGAR = "ng.ebrahimi@jabama.com"
 EMAIL_HOSSEIN = "h.mehdikhah@Alibaba.ir"
 EMAIL_AHMAD = "ahmad@jabama.ir"
 EMAIL_NIMA_EBRAHIMI = "n.ebrahimi@jabama.ir"
-EMAIL_NIMA_RASOULZADE = "n.rasoulzade@jabama.ir"
 EMAIL_MASOUD = "mm.shiri@Alibaba.ir"
 EMAIL_SAMA = "s.ostadi@Jabama.ir"
-EMAIL_MOHAMMAD_AMIRI = "Mh.Amiri@Jabama.com"
+EMAIL_RASHA = "rasha@jabama.com"
+EMAIL_KAMYAB = "k.farhadi@jabama.com"
--- metabot.py
+++ metabot.py
@@ -1,139 +1,222 @@
-import json
-import urllib.request
-
-import schedule
-import time
-
-import requests
-import os
-import env
-
-class Data:
-    METABASE_SESSION = ""
-def getMetabaseSession(username, password):
-    payload = {
-        "username": username,
-        "password": password
-    }
-    headers = {
-        'content-type': "application/json",
-    }
-    response = requests.request("POST", env.METABASE_API_GET_SESSION, data=json.dumps(payload, separators=(',', ':')), headers=headers)
-    Data.METABASE_SESSION = json.loads(response.text)['id']
-
-def getMetaCards(card_ids):
-    responses = []
-    for card_id in card_ids:
-        url = "http://metabase.jabama.com/api/card/" + card_id + "/query"
-
-        headers = {
-            'Content-Type': "application/json",
-            'X-Metabase-Session': Data.METABASE_SESSION,
-            }
-
-        responses.append(requests.request("POST", url, headers=headers))    
-    return responses
-
-def getDataFromResponses(responses):
-    responses_data = []
-    for response in responses:
-        cols = json.loads(response.text)['data']['cols']
-        rows = json.loads(response.text)['data']['rows']
-
-        col_titles = []
-        for col in cols:
-            col_titles.append(col['name'])
-
-        responses_data.append({
-            "columns": col_titles,
-            "rows": rows
-        })
-    return responses_data
-
-def formatMessageBlocks(response_tables):
-    message_blocks = []
-    for response_table in response_tables:
-        plain_text = ""
-        response_table.get("columns").pop(0)
-        for row in response_table.get("rows"):
-            plain_text = plain_text + "*" + str(row[0]) + "*\n"
-            indx = 1
-            for col in response_table.get("columns"):
-                plain_text = plain_text + col + ": " + str(row[indx]) + "\n"
-                indx = indx + 1
-            plain_text = plain_text + "\n"
-        message_blocks.append({
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": plain_text
-            }
-        })
-        message_blocks.append({
-            "type": "divider"
-        })
-    return message_blocks
-
-def getUserIDByEmail(emails):
-    responses = []
-    for email in emails:  
-        querystring = {
-            "email" : email
-        }
-        headers = {
-            'Content-Type': "application/json",
-            'Authorization': "Bearer " + env.METABOT_TOKEN,
-            }
-        response = requests.request("GET", env.SLACK_API_LOOK_UP_BY_EMAIL, headers=headers, params=querystring)
-        if json.loads(response.text)['ok']:
-            responses.append(json.loads(response.text)['user']['id'])
-    return responses
-        
-def sendMessagesToSlackUsers(user_ids, message_blocks):
-    responses = []
-    for user_id in user_ids:
-        msg_indx = 0
-        payload = {
-        "channel" : user_id,
-        "blocks": message_blocks,
-        "as_user": True
-        }
-        headers = {
-            'Content-Type': "application/json",
-            'Authorization': "Bearer " + env.METABOT_TOKEN,
-            }
-        response = requests.request("POST", env.SLACK_API_POST_MESSAGE, data=str(payload), headers=headers)
-        responses.append({
-            'user_id': user_id,
-            'message_index': msg_indx,
-            'status': json.loads(response.text)['ok']
-        })
-        msg_indx = msg_indx + 1
-    return responses
-
-def metabot_job():
-    metacard_responses = getMetaCards([env.CARD_ID_ACCOMMODATION, env.CARD_ID_TOTAL, env.CARD_ID_SEARCH])
-    responses_data = getDataFromResponses(metacard_responses)
-    message_blocks = formatMessageBlocks(responses_data)
-    user_ids = getUserIDByEmail([
-        env.EMAIL_HOSSEIN,
-        env.EMAIL_AHMAD,
-        env.EMAIL_NIMA_EBRAHIMI,
-        env.EMAIL_NIMA_RASOULZADE,
-        env.EMAIL_MASOUD,
-        env.EMAIL_SAMA,
-        env.EMAIL_MOHAMMAD_AMIRI
-    ])
-    send_messages_status = sendMessagesToSlackUsers(user_ids, message_blocks)
-def metabase_get_session_job():
-    getMetabaseSession(env.METABASE_USERNAME, env.METABASE_PASSWORD)
-
-schedule.every().hour.do(metabot_job)
-schedule.every().day.at("23:59").do(metabot_job)
-schedule.every().monday.do(metabase_get_session_job)
-
-metabase_get_session_job()
-
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 17,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import json\n",
+    "import urllib.request\n",
+    "\n",
+    "import schedule\n",
+    "import time\n",
+    "\n",
+    "import requests\n",
+    "import os\n",
+    "import env\n",
+    "\n",
+    "class Data:\n",
+    "    METABASE_SESSION = \"\"\n",
+    "def getMetabaseSession(username, password):\n",
+    "    payload = {\n",
+    "        \"username\": username,\n",
+    "        \"password\": password\n",
+    "    }\n",
+    "    headers = {\n",
+    "        'content-type': \"application/json\",\n",
+    "    }\n",
+    "    response = requests.request(\"POST\", env.METABASE_API_GET_SESSION, data=json.dumps(payload, separators=(',', ':')), headers=headers)\n",
+    "    Data.METABASE_SESSION = json.loads(response.text)['id']\n",
+    "\n",
+    "def getMetaCards(card_ids):\n",
+    "    responses = []\n",
+    "    for card_id in card_ids:\n",
+    "        url = \"http://metabase.jabama.com/api/card/\" + card_id + \"/query\"\n",
+    "\n",
+    "        headers = {\n",
+    "            'Content-Type': \"application/json\",\n",
+    "            'X-Metabase-Session': Data.METABASE_SESSION,\n",
+    "            }\n",
+    "\n",
+    "        responses.append(requests.request(\"POST\", url, headers=headers))    \n",
+    "    return responses\n",
+    "\n",
+    "def getDataFromResponses(responses):\n",
+    "    responses_data = []\n",
+    "    for response in responses:\n",
+    "        cols = json.loads(response.text)['data']['cols']\n",
+    "        rows = json.loads(response.text)['data']['rows']\n",
+    "\n",
+    "        col_titles = []\n",
+    "        for col in cols:\n",
+    "            col_titles.append(col['name'])\n",
+    "\n",
+    "        responses_data.append({\n",
+    "            \"columns\": col_titles,\n",
+    "            \"rows\": rows\n",
+    "        })\n",
+    "    return responses_data\n",
+    "\n",
+    "def formatMessageBlocks(response_tables):\n",
+    "    message_blocks = []\n",
+    "    for response_table in response_tables:\n",
+    "        plain_text = \"\"\n",
+    "        response_table.get(\"columns\").pop(0)\n",
+    "        for row in response_table.get(\"rows\"):\n",
+    "            plain_text = plain_text + \"*\" + str(row[0]) + \"*\\n\"\n",
+    "            indx = 1\n",
+    "            for col in response_table.get(\"columns\"):\n",
+    "                plain_text = plain_text + col + \": \" + str(row[indx]) + \"\\n\"\n",
+    "                indx = indx + 1\n",
+    "            plain_text = plain_text + \"\\n\"\n",
+    "        message_blocks.append({\n",
+    "            \"type\": \"section\",\n",
+    "            \"text\": {\n",
+    "                \"type\": \"mrkdwn\",\n",
+    "                \"text\": plain_text\n",
+    "            }\n",
+    "        })\n",
+    "        message_blocks.append({\n",
+    "            \"type\": \"divider\"\n",
+    "        })\n",
+    "    return message_blocks\n",
+    "\n",
+    "def getUserIDByEmail(emails):\n",
+    "    responses = []\n",
+    "    for email in emails:  \n",
+    "        querystring = {\n",
+    "            \"email\" : email\n",
+    "        }\n",
+    "        headers = {\n",
+    "            'Content-Type': \"application/json\",\n",
+    "            'Authorization': \"Bearer \" + env.METABOT_TOKEN,\n",
+    "            }\n",
+    "        response = requests.request(\"GET\", env.SLACK_API_LOOK_UP_BY_EMAIL, headers=headers, params=querystring)\n",
+    "        if json.loads(response.text)['ok']:\n",
+    "            responses.append(json.loads(response.text)['user']['id'])\n",
+    "    return responses\n",
+    "        \n",
+    "def sendMessagesToSlackUsers(user_ids, message_blocks):\n",
+    "    responses = []\n",
+    "    for user_id in user_ids:\n",
+    "        msg_indx = 0\n",
+    "        payload = {\n",
+    "        \"channel\" : user_id,\n",
+    "        \"blocks\": message_blocks,\n",
+    "        \"as_user\": True\n",
+    "        }\n",
+    "        headers = {\n",
+    "            'Content-Type': \"application/json\",\n",
+    "            'Authorization': \"Bearer \" + env.METABOT_TOKEN,\n",
+    "            }\n",
+    "        response = requests.request(\"POST\", env.SLACK_API_POST_MESSAGE, data=str(payload), headers=headers)\n",
+    "        responses.append({\n",
+    "            'user_id': user_id,\n",
+    "            'message_index': msg_indx,\n",
+    "            'status': json.loads(response.text)['ok']\n",
+    "        })\n",
+    "        msg_indx = msg_indx + 1\n",
+    "    return responses\n",
+    "\n",
+    "def metabot_job():\n",
+    "    metacard_responses = getMetaCards([env.CARD_ID_ACCOMMODATION_BY_VENUE, env.CARD_ID_TOTAL, env.CARD_ID_SEARCH, env.CARD_ID_ORDER_STATUS])\n",
+    "    responses_data = getDataFromResponses(metacard_responses)\n",
+    "    message_blocks = formatMessageBlocks(responses_data)\n",
+    "    user_ids = getUserIDByEmail([\n",
+    "        env.EMAIL_HOSSEIN,\n",
+    "        env.EMAIL_AHMAD,\n",
+    "        env.EMAIL_NIMA_EBRAHIMI,\n",
+    "        env.EMAIL_NIMA_RASOULZADE,\n",
+    "        env.EMAIL_MASOUD,\n",
+    "        env.EMAIL_SAMA,\n",
+    "        env.EMAIL_MOHAMMAD_AMIRI\n",
+    "    ])\n",
+    "    send_messages_status = sendMessagesToSlackUsers(user_ids, message_blocks)\n",
+    "    \n",
+    "def metabase_get_session_job():\n",
+    "    getMetabaseSession(env.METABASE_USERNAME, env.METABASE_PASSWORD)\n",
+    "\n",
+    "schedule.every().day.at(\"08:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"09:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"10:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"11:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"12:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"13:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"14:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"15:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"16:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"17:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"18:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"19:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"20:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"21:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"22:59\").do(metabot_job)\n",
+    "schedule.every().day.at(\"23:59\").do(metabot_job)\n",
+    "schedule.every().monday.do(metabase_get_session_job)\n",
+    "\n",
+    "metabase_get_session_job()\n",
+    "\n",
+    "while 1:\n",
+    "    schedule.run_pending()\n",
+    "    time.sleep(1)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
