b2f0c21fa812ef61f9654d03ddbbf616017835fd
negaR Ebrahimi <negarfreebots@gmail.com>
2020-06-28 21:38:33 +0430

    Limit the metabot to include the THC report to some recipients and check its dynamic reporting (send what to whom) functionality as well.

---
+++ THC_calculation.py
@@ -0,0 +1,185 @@
+import json
+import requests
+import env
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+class Data:
+    METABASE_SESSION = ""
+
+def getMetaCards(card_ids):
+    responses = []
+    for card_id in card_ids:
+        url = "http://metabase.jabama.com/api/card/" + str(card_id) + "/query"
+
+        headers = {
+            'Content-Type': "application/json",
+            'X-Metabase-Session': Data.METABASE_SESSION,
+            }
+
+        responses.append([card_id, requests.request("POST", url, headers=headers)])
+    return responses
+
+def getDataFromPivotResponses(responses):
+    responses_data = []
+    
+    for response in responses:
+        cols = json.loads(response[1].text)['data']['cols']
+        rows = json.loads(response[1].text)['data']['rows']
+        card_id = response[0]
+        col_titles = []
+        for col in cols:
+            col_titles.append(col['name'])
+
+        responses_data.append({
+            "card_id": card_id,
+            "columns": col_titles,
+            "rows": rows
+        })
+    return responses_data
+
+def transformPivotResponseToDataFrame(pivotResponse):
+    job_audit_df = pd.DataFrame(
+        pivotResponse.get("rows"),
+        columns = pivotResponse.get("columns")
+    )[
+        [
+            "id",
+            "OrderId",
+            "PlaceCategory",
+            "WorkflowStepId",
+            "previous_workflowstepid",
+            "next_workflowstepid",
+            "nth",
+            "CreatedDate",
+            "UpdatedDate",
+            "UpdatedBy"
+        ]
+    ]
+    return job_audit_df
+
+def extractPrebookOrders(job_audit_df):
+    prebook_orders_list = []
+    prebook_flag = 0
+    for indx, job_audit in job_audit_df.iterrows():
+        
+        if job_audit_df["nth"].iloc[indx] == 1:
+            if prebook_flag == 1:
+                prebook_flag = 0
+                prebook_orders_list.append(prebook_list)
+            if job_audit_df["WorkflowStepId"].iloc[indx] == "init" \
+            and job_audit_df["next_workflowstepid"].iloc[indx] == "init":
+                prebook_flag = 1
+                prebook_list = []
+        if prebook_flag == 1:
+            prebook_list.append(job_audit)
+    return prebook_orders_list
+    
+def extractInstantOrders(job_audit_df):
+    instant_orders_list = []
+    instant_flag = 0
+    for indx, job_audit in job_audit_df.iterrows():     
+        if job_audit_df["nth"].iloc[indx] == 1:
+            if instant_flag == 1:
+                instant_flag = 0
+                instant_orders_list.append(instant_list)
+            if job_audit_df["WorkflowStepId"].iloc[indx] == "init" \
+            and job_audit_df["next_workflowstepid"].iloc[indx] != "init":
+                instant_flag = 1
+                instant_list = []
+        if instant_flag == 1:
+            instant_list.append(job_audit)
+    return instant_orders_list
+    
+def splitOrderPathToSubPaths(prebook_batches):
+    prebook_splitted_batches = []
+    for prebook_batch in prebook_batches:
+            being_splitted_list = []
+            counting_off_flag = 0
+            for job_audit in prebook_batch:
+                if counting_off_flag == 0:
+                    being_splitted_list.append(job_audit)
+                if job_audit["WorkflowStepId"] == "payment" \
+                or job_audit["WorkflowStepId"] == "supplydecline":
+                    counting_off_flag = 1
+                if (job_audit["WorkflowStepId"] == "paymenttimeout" \
+                    and job_audit["next_workflowstepid"] == "init"):
+                    prebook_splitted_batches.append(being_splitted_list)
+                    being_splitted_list = []
+                    counting_off_flag = 0
+            prebook_splitted_batches.append(being_splitted_list)
+                
+    return prebook_splitted_batches
+
+def inferTheStepUserTypeFromJobAudit(job_audit):
+    StepUserType = "Jabama"
+    if not (job_audit["WorkflowStepId"] == "payment" \
+    or job_audit["WorkflowStepId"] == "supplydecline"):
+        return StepUserType
+    if job_audit["UpdatedBy"] == "" \
+    or (("jabama" in job_audit["UpdatedBy"] \
+    or "Jabama" in job_audit["UpdatedBy"] \
+    or job_audit["UpdatedBy"][0] == "0" \
+    or job_audit["UpdatedBy"][0] == "+") \
+    and not(job_audit["UpdatedBy"][-10:] == "alibaba.ir" \
+            or job_audit["UpdatedBy"][-10:] == "Alibaba.ir")):
+        StepUserType = "User"
+    return StepUserType
+  
+def calculateTimeToHostConclusion_df(prebook_splitted_batches):
+    host_conclusion_prebook_splitted_batches = pd.DataFrame(dtype=float, columns = ["sub_order", "THC", "HostConcludingUserType", "HostConclusionType"])
+    for prebook_splitted_batch in prebook_splitted_batches:
+        start_dt = prebook_splitted_batch[0]["CreatedDate"]
+        if start_dt[22] == "+":
+            start_dt = start_dt[:22] + '0' + start_dt[22:]
+        if start_dt[21] == "+":
+            start_dt = start_dt[:21] + '00' + start_dt[21:]
+        if start_dt[19] == "+":
+            start_dt = start_dt[:19] + '.000' + start_dt[19:]
+            
+        end_dt = prebook_splitted_batch[-1]["UpdatedDate"]
+        if end_dt[22] == "+":
+            end_dt = end_dt[:22] + '0' + end_dt[22:]
+        if end_dt[21] == "+":
+            end_dt = end_dt[:21] + '00' + end_dt[21:]
+        if end_dt[19] == "+":
+            end_dt = end_dt[:19] + '.000' + end_dt[19:]
+  
+        THC = datetime.fromisoformat(end_dt) - datetime.fromisoformat(start_dt)
+        ConcludingUserType = inferTheStepUserTypeFromJobAudit(prebook_splitted_batch[-1])
+        
+        ConclusionType = "Decline"
+        if prebook_splitted_batch[-1]["WorkflowStepId"] == "payment":
+            ConclusionType = "Payment"
+            
+        host_conclusion_prebook_splitted_batches = host_conclusion_prebook_splitted_batches.append({
+         "sub_order": prebook_splitted_batch,
+         "THC": (THC.seconds//60)%60,
+         "HostConcludingUserType": ConcludingUserType,
+         "HostConclusionType": ConclusionType
+        }, ignore_index=True)
+    return host_conclusion_prebook_splitted_batches
+
+def host_conclusion_job():
+    #Get and store the raw data of union of questions from Metabase.
+    pivot_metacard_responses = getMetaCards([env.CARD_ID_JOBAUDIT])
+    pivot_responses_data = getDataFromPivotResponses(pivot_metacard_responses)
+    job_audit_df = transformPivotResponseToDataFrame(pivot_responses_data[0])
+    
+    prebook_order_batches_list = extractPrebookOrders(job_audit_df)
+    instant_order_batches_list = extractInstantOrders(job_audit_df)
+
+    prebook_splitted_batches = splitOrderPathToSubPaths(prebook_order_batches_list)
+    prebook_splitted_batches_with_THC = calculateTimeToHostConclusion_df(prebook_splitted_batches)
+
+    total_prebook_splitted_batches = len(prebook_splitted_batches_with_THC)
+    THC_aggregated_df = prebook_splitted_batches_with_THC.groupby(["HostConcludingUserType", "HostConclusionType"], as_index = False).agg([np.median, 'count'])
+    THC_aggregated_df.columns = THC_aggregated_df.columns.droplevel(0)
+    THC_aggregated_df = THC_aggregated_df.reset_index()[["HostConcludingUserType", "HostConclusionType", "median", "count"]]
+    THC_aggregated_df = THC_aggregated_df.rename(columns={"median": "THC_median", "count": "SubOrders_Count"})
+    THC_aggregated_df["Percentage"] = THC_aggregated_df["SubOrders_Count"] / total_prebook_splitted_batches * 100
+    THC_aggregated_df["Percentage"] = THC_aggregated_df["Percentage"].map("{:,.2f}%".format)
+    
+    THC_result_df = THC_aggregated_df[["HostConcludingUserType", "HostConclusionType", "Percentage", "THC_median"]]
+    return THC_result_df
--- metabot.py
+++ metabot.py
@@ -8,6 +8,8 @@ import time
 import requests
 import os
 import env
+import THC_calculation as thc
+import pandas as pd
 
 class Data:
     METABASE_SESSION = ""
@@ -25,21 +27,23 @@ def getMetabaseSession(username, password):
 def getMetaCards(card_ids):
     responses = []
     for card_id in card_ids:
-        url = "http://metabase.jabama.com/api/card/" + card_id + "/query"
+        url = "http://metabase.jabama.com/api/card/" + str(card_id) + "/query"
 
         headers = {
             'Content-Type': "application/json",
             'X-Metabase-Session': Data.METABASE_SESSION,
             }
 
-        responses.append(requests.request("POST", url, headers=headers))
+        responses.append([card_id, requests.request("POST", url, headers=headers)])
     return responses
 
 def getDataFromTableResponses(responses):
     responses_data = []
     for response in responses:
-        rows = json.loads(response.text)['data']['rows']
+        rows = json.loads(response[1].text)['data']['rows']
+        card_id = response[0]
         responses_data.append({
+            "card_id": card_id,
             "rows": rows
         })
         
@@ -49,27 +53,32 @@ def getDataFromPivotResponses(responses):
     responses_data = []
     
     for response in responses:
-        cols = json.loads(response.text)['data']['cols']
-        rows = json.loads(response.text)['data']['rows']
+        cols = json.loads(response[1].text)['data']['cols']
+        rows = json.loads(response[1].text)['data']['rows']
+        card_id = response[0]
         col_titles = []
         for col in cols:
             col_titles.append(col['name'])
 
         responses_data.append({
+            "card_id": card_id,
             "columns": col_titles,
             "rows": rows
         })
     return responses_data
 
-def formatMessageBlocks(pivot_responses, table_responses = []):
+def formatMessageBlocks(pivot_responses = [], table_responses = []):
     message_blocks = []
     for pivot_response in pivot_responses:
         plain_text = ""
-        pivot_response.get("columns").pop(0)
+        columns = copy.deepcopy(pivot_response).get("columns")
+        if columns == []:
+            columns = ['']
+        columns.pop(0)
         for row in pivot_response.get("rows"):
             plain_text = plain_text + "*" + str(row[0]) + "*\n"
             indx = 1
-            for col in pivot_response.get("columns"):
+            for col in columns:
                 plain_text = plain_text + col + ": " + str(row[indx]) + "\n"
                 indx = indx + 1
             plain_text = plain_text + "\n"
@@ -83,7 +92,6 @@ def formatMessageBlocks(pivot_responses, table_responses = []):
         message_blocks.append({
             "type": "divider"
         })
-        
 
     for table_response in table_responses:
         plain_text = ""
@@ -104,69 +112,112 @@ def formatMessageBlocks(pivot_responses, table_responses = []):
         })
     return message_blocks
 
-def getUserIDByEmail(emails):
-    responses = []
-    for email in emails:  
-        querystring = {
-            "email" : email
+def getUserIDByEmail(email):
+    querystring = {
+        "email" : email
+    }
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Bearer " + env.METABOT_TOKEN,
         }
-        headers = {
-            'Content-Type': "application/json",
-            'Authorization': "Bearer " + env.METABOT_TOKEN,
-            }
-        response = requests.request("GET", env.SLACK_API_LOOK_UP_BY_EMAIL, headers=headers, params=querystring)
-        if json.loads(response.text)['ok']:
-            responses.append(json.loads(response.text)['user']['id'])
-    return responses
+    response = requests.request("GET", env.SLACK_API_LOOK_UP_BY_EMAIL, headers=headers, params=querystring)
+    if json.loads(response.text)['ok']:
+        response = json.loads(response.text)['user']['id']
+    return response
         
-def sendMessagesToSlackUsers(user_ids, message_blocks):
+def sendMessagesToSlackUser(user_id, message_blocks):
     responses = []
-    for user_id in user_ids:
-        msg_indx = 0
-        payload = {
-        "channel" : user_id,
-        "blocks": message_blocks,
-        "as_user": True
+    msg_indx = 0
+    payload = {
+    "channel" : user_id,
+    "blocks": message_blocks,
+    "as_user": True
+    }
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Bearer " + env.METABOT_TOKEN,
         }
-        headers = {
-            'Content-Type': "application/json",
-            'Authorization': "Bearer " + env.METABOT_TOKEN,
-            }
-        response = requests.request("POST", env.SLACK_API_POST_MESSAGE, data=str(payload), headers=headers)
-        responses.append({
-            'user_id': user_id,
-            'message_index': msg_indx,
-            'status': json.loads(response.text)['ok']
-        })
-        msg_indx = msg_indx + 1
+    response = requests.request("POST", env.SLACK_API_POST_MESSAGE, data=str(payload), headers=headers)
+    responses.append({
+        'user_id': user_id,
+        'status': json.loads(response.text)['ok']
+    })
     return responses
 
+def TransformDataframeToTableReponse(df):
+    list_of_rows = []
+    for index, row in df.iterrows():
+        list_of_rows.append(row)
+    table_response = {'card_id': 0, 'rows': list_of_rows}
+    return table_response
+
 def metabot_job():
-    pivot_metacard_responses = getMetaCards([
-        env.CARD_ID_ACCOMMODATION,
-        env.CARD_ID_ACCOMMODATION_BY_VENUE,
-        env.CARD_ID_TOTAL,
-        env.CARD_ID_SEARCH_CR
-    ])
+    #Get the recipients' list from a MetaCard, extract the list of their email addresses and
+    #their personalized question cardIDs from it.
+    recipients_metacard = getMetaCards([
+        env.CARD_ID_METABOT_RECIPIENTS
+    ])   
+    send_what_to_whom = [
+        [
+            recipient[0],
+            recipient[1],
+            [] if recipient[2] == '' else [int(pivot_question_cardIDs) for pivot_question_cardIDs in recipient[2].split(',')],
+            [] if recipient[3] == '' else [int(table_question_cardIDs) for table_question_cardIDs in recipient[3].split(',')]
+        ] for recipient in (getDataFromTableResponses(recipients_metacard)[0]['rows'])]
+    #End of getting the recipients' list
+    
+    #Get the union list of pivot question cardIDs and table question cardIDs.
+    unified_pivot_cardIDs = list(set().union(*[send_what_to_a_recipient[2] for send_what_to_a_recipient in send_what_to_whom]))
+    unified_table_cardIDs = list(set().union(*[send_what_to_a_recipient[3] for send_what_to_a_recipient in send_what_to_whom]))
+    #End of getting the union of questions' cardIDs.
+    
+    #Get and store the raw data of union of questions from Metabase.
+    pivot_metacard_responses = getMetaCards(unified_pivot_cardIDs)
     pivot_responses_data = getDataFromPivotResponses(pivot_metacard_responses)
-    
-    table_metacard_responses = getMetaCards([
-        env.CARD_ID_ORDER_STATUS
-    ])
+    table_metacard_responses = getMetaCards(unified_table_cardIDs)
     table_responses_data = getDataFromTableResponses(table_metacard_responses)
+    #End of creating the message blocks
     
-    message_blocks = formatMessageBlocks(pivot_responses_data, table_responses_data)
+    #Get the recipients' Slack ID by their emails and send their personalized message blocks to them.
+    for send_what_to_a_recipient in send_what_to_whom:
+        user_id = getUserIDByEmail(send_what_to_a_recipient[1])
+        pivot_question_cardIDs = send_what_to_a_recipient[2]
+        table_question_cardIDs = send_what_to_a_recipient[3]
+        if user_id == [] or (pivot_question_cardIDs == [] and table_question_cardIDs == []):
+            return
+        message_blocks= []
+        #Extract the user's personalized question responses to be included in their message block
+        this_user_pivot_responses_data = []
+        for pivot_question_cardID in pivot_question_cardIDs:
+            for pivot_response_data in pivot_responses_data:
+                if pivot_response_data['card_id'] == pivot_question_cardID:
+                    this_user_pivot_responses_data.append(pivot_response_data)                   
+        this_user_table_responses_data = []
+        for table_question_cardID in table_question_cardIDs:
+            for table_response_data in table_responses_data:
+                if table_response_data['card_id'] == table_question_cardID:
+                    this_user_table_responses_data.append(table_response_data)
+        #End of extracting this user's personalized question responses 
+        
+        #Include the THC report in this recipient's responses data if his name is among those who should receive it.
+        if send_what_to_a_recipient[0] in ['EMAIL_AHMAD', 'EMAIL_HOSSEIN', 'EMAIL_MOHSEN']:
+            thc.Data.METABASE_SESSION = Data.METABASE_SESSION
+            thc_result_df = thc.host_conclusion_job()
+            thc_table_response = TransformDataframeToTableReponse(thc_result_df)
+            this_user_table_responses_data.append(thc_table_response)
+        #End of including the THC report in this recipient's responses data
+
+        email = send_what_to_a_recipient[1]
+        user_id = getUserIDByEmail(email)
+
+        message_blocks = formatMessageBlocks(
+            this_user_pivot_responses_data,
+            this_user_table_responses_data
+        )
+   
+        send_messages_status = sendMessagesToSlackUser(user_id, message_blocks)
+    #End of sending the message blocks to recipients
     
-    user_ids = getUserIDByEmail([
-        env.EMAIL_HOSSEIN,
-        env.EMAIL_AHMAD,
-        env.EMAIL_NIMA_EBRAHIMI,
-        env.EMAIL_MASOUD,
-        env.EMAIL_RASHA,
-        env.EMAIL_KAMYAB,
-        env.EMAIL_MOHSEN
-    ])
-    send_messages_status = sendMessagesToSlackUsers(user_ids, message_blocks)
     
 def metabase_get_session_job():
     getMetabaseSession(env.METABASE_USERNAME, env.METABASE_PASSWORD)
@@ -194,4 +245,3 @@ metabase_get_session_job()
 while 1:
     schedule.run_pending()
     time.sleep(1)
-
