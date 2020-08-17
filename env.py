7c8e6cc52f44bee9f69651b3262c918c58c90091
Negar Ebrahimi <negarfreebots@gmail.com>
2020-08-17 15:12:24 +0430

    Update env.py

--- env.py
+++ env.py
@@ -14,13 +14,3 @@ METABASE_API_GET_SESSION = "http://metabase.jabama.com/api/session"
 
 SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 SLACK_API_LOOK_UP_BY_EMAIL = "https://slack.com/api/users.lookupByEmail"
-
-#Email addresses have been given to the Metabot dynamically.
-# EMAIL_NEGAR = "ng.ebrahimi@jabama.com"
-#EMAIL_HOSSEIN = "h.mehdikhah@Alibaba.ir"
-#EMAIL_AHMAD = "ahmad@jabama.ir"
-#EMAIL_NIMA = "n.ebrahimi@jabama.ir"
-#EMAIL_SAMA = "s.ostadi@Jabama.ir"
-#EMAIL_RASHA = "rasha@jabama.com"
-#EMAIL_KAMYAB = "k.farhadi@jabama.com"
-#EMAIL_MOHSEN = "m.seifan@jabama.com"
